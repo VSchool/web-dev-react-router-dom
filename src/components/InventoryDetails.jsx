368a6a2b68c4f2665f62bbb98e5310528de3a034
Jordan Burger <jordanburger22@gmail.com>
2024-06-11 21:39:20 +0000

    updated inventory details component

--- src/components/InventoryDetails.jsx
+++ src/components/InventoryDetails.jsx
@@ -1,17 +1,12 @@
-import { useParams } from "react-router-dom";
-import { inventory } from "../inventory";
+
 
 function InventoryDetails() {
 
-    const {id} = useParams()
-    
-    const foundItem = inventory.find(item => item.id === id)
+
 
     return ( 
         <div>
-            <h1>{foundItem.name}</h1>
-            <h1>{foundItem.price}</h1>
-            <h1>{foundItem.quantity}</h1>
+            
         </div>
      );
 }
