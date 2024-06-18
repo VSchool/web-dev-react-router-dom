9a527c48044f7c8ed61c831e74ddaf8b86435632
Jordan Burger <jordanburger22@gmail.com>
2024-06-18 17:54:44 +0000

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/inventory' element={<InventoryPage />}/>
+        <Route path='/inventory' element={<InventoryPage />}/>
       </Routes>
     </>
   )
