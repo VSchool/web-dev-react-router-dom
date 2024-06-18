522b0163afbb21e981d9d016dd70b2a8eb209bb7
Jordan Burger <jordanburger22@gmail.com>
2024-06-18 17:53:57 +0000

    added router, routes, route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import InventoryPage from './components/InventoryPage'
-
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
       <Navbar />
-      <Home />
-      <InventoryPage />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/inventory' element={<InventoryPage />}/>
+      </Routes>
     </>
   )
 }
