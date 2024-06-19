08a0271dd332f987ba65d55cd388d8434a6e6845
Zak Ward <zakward85@gmail.com>
2024-06-19 10:28:38 -0500

    useParams

--- src/components/InventoryDetails.jsx
+++ src/components/InventoryDetails.jsx
@@ -1,12 +1,22 @@
-
+import { inventory } from "../inventory";
+import { useParams } from "react-router-dom";
 
 function InventoryDetails() {
 
+    
+  const {id} = useParams()
 
+  const foundItem = inventory.find(item => item.id === id)
+
+  console.log(foundItem)
 
     return ( 
         <div>
-            inventory details
+            <h2>{id}</h2>
+            <h2>Name: {foundItem.name}</h2>
+            <h3>Category : {foundItem.category}</h3>
+            <p>Price: {foundItem.price}</p>
+            <p>Quantity: {foundItem.quantity}</p>
         </div>
      );
 }
